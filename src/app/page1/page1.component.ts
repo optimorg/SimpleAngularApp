7efc7931243afb767354badb80548bba0a513004
Green, Morgan [USA] <Green_Morgan@bah.com>
2023-04-05 12:37:21 -0400

    "New folders"

---
+++ src/app/page1/page1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page1',
+  templateUrl: './page1.component.html',
+  styleUrls: ['./page1.component.css']
+})
+export class Page1Component implements OnInit {
+  
+  pageName = 'Page 1';
+
+  constructor() {}
+  
+  ngOnInit() {
+    setTimeout(  () => {  this.pageName = 'First Page' }, 5000);
+
+  }
+
+}
